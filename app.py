e723f73258c0b633758c384990f90907639bd556
Alexandr Larionov <alarionov93@yandex.ru>
2021-08-14 20:47:55 +0500

    added comments, added prints of geo objects from geo coder to choose

--- app.py
+++ app.py
@@ -33,13 +33,16 @@ def get_coords(res_pts, idx, q):
 		res = requests.get("https://geocode-maps.yandex.ru/1.x?apikey=063a0509-ec40-4683-b6e3-cdd341b15416&geocode=%s&results=5&format=json" % res_pts[idx], headers={'User-Agent': 'curl/7.78.0', 'Accept': '*/*'}).json()
 	except IndexError:
 		print(idx, res_pts)
-		raise Exception("AAAAAAAAAAAA")
+		raise Exception("[ERROR]: Index of res_pts is out of range!")
 	for lst_elem in parse_dict(res):
 		try:
 			kind = lst_elem['GeoObject']['metaDataProperty']['GeocoderMetaData']['kind']
 			name = lst_elem['GeoObject']['name']
 			if kind != 'hydro':
 				q.put((idx, [lst_elem['GeoObject']['Point']['pos'].split(' ')[1], lst_elem['GeoObject']['Point']['pos'].split(' ')[0]]))
+				print(name)
+				print("Lat: %s, Lon: %s" % (lst_elem['GeoObject']['Point']['pos'].split(' ')[1], lst_elem['GeoObject']['Point']['pos'].split(' ')[0]))
+			else:
 		except KeyError:
 			print("[ERROR]: Can not get some keys!")
 
@@ -56,7 +59,7 @@ def points():
 	idxs = []
 	for p in p_lst:
 		try:
-			for ref_pt in struct[p]:
+			for ref_pt in struct[p]: # one name in p_lst CAN containt one OR MORE points in data.json: that's why there's a for loop
 				res_pts.append([ref_pt["coord"].split(',')[0], ref_pt["coord"].split(',')[1].strip()])
 		except KeyError:
 			res_pts.append(p)
@@ -66,6 +69,7 @@ def points():
 
 	threads = []
 	q = Queue()
+	# threads works not with the whole res_pts, but with only one element separetly / indexes are different and should not repeat
 	for idx in idxs:
 		threads.append(Thread(target=get_coords, args=(res_pts, idx, q)))
 		threads[-1].start()
